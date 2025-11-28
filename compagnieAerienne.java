8ac04a776483e6ada0ae468af46f171ca149c731
Amira <amira.val806@gmail.com>
2025-11-28 12:08:02 +0100

    Add CompagnieAerienne class with basic methods

--- compagnieAerienne.java
+++ compagnieAerienne.java
@@ -1,1 +1,39 @@
 
+public class CompagnieAerienne {
+    
+  
+    private String nom;
+    private String codeIATA; 
+    
+
+    public CompagnieAerienne(String nom, String codeIATA) {
+        this.nom = nom;
+        this.codeIATA = codeIATA;
+    }
+    
+
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCodeIATA() {
+        return codeIATA;
+    }
+    
+  
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setCodeIATA(String codeIATA) {
+        this.codeIATA = codeIATA;
+    }
+    
+
+
+    public String toString() {
+        return nom + " (" + codeIATA + ")";
+    }
+}
+
