8176c4afd9ad5c6cc48842678c0be1a86a1c4e16
Amira <amira.val806@gmail.com>
2025-11-28 12:54:53 +0100

    Update compagnieAerienne.java + commentaires

--- compagnieAerienne.java
+++ compagnieAerienne.java
@@ -1,18 +1,17 @@
-
 public class CompagnieAerienne {
     
-  
+    // les attributs sont privés pour l'encapsulation (on ne peut pas y accéder directement)
     private String nom;
-    private String codeIATA; 
+    private String codeIATA; // ceci est le code unique à deux lettres de la compagnie (indispensable en aviation commerciale)
     
-
-    public CompagnieAerienne(String nom, String codeIATA) {
+    // constructeur : il permet de créer un objet CompagnieAerienne et de définir
+    // son nom et son code dès sa création
+    public CompagnieAerienne(String nom, String codeIATA) { 
         this.nom = nom;
         this.codeIATA = codeIATA;
     }
     
-
-    
+    // les "getters" (accesseurs) permettent de lire la valeur des attributs privés
     public String getNom() {
         return nom;
     }
@@ -21,7 +20,7 @@ public class CompagnieAerienne {
         return codeIATA;
     }
     
-  
+    // les "setters" (mutateurs) permettent de modifier la valeur des attributs privés
     public void setNom(String nom) {
         this.nom = nom;
     }
@@ -30,10 +29,8 @@ public class CompagnieAerienne {
         this.codeIATA = codeIATA;
     }
     
-
-
+    // la méthode toString() retourne une description de l'objet pour l'affichage
     public String toString() {
         return nom + " (" + codeIATA + ")";
     }
 }
-
