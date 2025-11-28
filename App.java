13f5486836e958cf2010c0eabbf8b67e5f1fb6ba
Amira <amira.val806@gmail.com>
2025-11-28 12:01:20 +0100

    Remove App class and main method
    Removed the entire App class implementation.

--- App.java
+++ App.java
@@ -1,15 +1,1 @@
-package flightbookingsystem;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        
-        Vol v = new Vol("AF123", "AirTest", "Paris", "Londres", "2025-12-01T09:00", 150);
-        Passager p = new Passager(1, "Dupont", "0600000000");
-        boolean added = v.ajouterPassager(p);
-        System.out.println(v);
-        System.out.println("Passager ajouté: " + added + ", nombre passagers=" + v.nombrePassagers());
-    }
-}
 
